93c07b20e29275e07360fd0e93a4c2b3d87393fe
Davyd Kohut <davyd.kohut@gmail.com>
2022-11-26 02:06:02 +0200

    0.1.2: more types

--- src/main.js
+++ src/main.js
@@ -1,16 +1,82 @@
 import { parse } from "./parser/index.js";
 import { Context, JuanaDocument } from "./context.js";
-import { arrayType, binaryType, floatType, intType, nullType, stringType } from "./types.js";
+import {
+    anyType,
+    arrayType,
+    binaryType,
+    boolType,
+    float128Type,
+    float256Type,
+    float32Type,
+    float512Type,
+    float64Type,
+    floatType,
+    int128Type,
+    int256Type,
+    int32Type,
+    int512Type,
+    int64Type,
+    intType,
+    nullType,
+    numberType,
+    objectType,
+    stringType,
+    ufloat128Type,
+    ufloat256Type,
+    ufloat32Type,
+    ufloat512Type,
+    ufloat64Type,
+    ufloatType,
+    uint128Type,
+    uint256Type,
+    uint32Type,
+    uint512Type,
+    uint64Type,
+    uintType,
+    unumberType
+} from "./types.js";
 import { namespaceHandler } from "./handlers.js";
 
 const globalContext = new Context();
 
 globalContext.putType(binaryType);
-globalContext.putType(floatType);
-globalContext.putType(intType);
 globalContext.putType(stringType);
 globalContext.putType(arrayType);
 globalContext.putType(nullType);
+globalContext.putType(boolType);
+globalContext.putType(anyType);
+globalContext.putType(objectType);
+
+globalContext.putType(numberType);
+globalContext.putType(unumberType);
+
+globalContext.putType(floatType);
+globalContext.putType(float32Type);
+globalContext.putType(float64Type);
+globalContext.putType(float128Type);
+globalContext.putType(float256Type);
+globalContext.putType(float512Type);
+
+globalContext.putType(ufloatType);
+globalContext.putType(ufloat32Type);
+globalContext.putType(ufloat64Type);
+globalContext.putType(ufloat128Type);
+globalContext.putType(ufloat256Type);
+globalContext.putType(ufloat512Type);
+
+globalContext.putType(intType);
+globalContext.putType(int32Type);
+globalContext.putType(int64Type);
+globalContext.putType(int128Type);
+globalContext.putType(int256Type);
+globalContext.putType(int512Type);
+
+globalContext.putType(uintType);
+globalContext.putType(uint32Type);
+globalContext.putType(uint64Type);
+globalContext.putType(uint128Type);
+globalContext.putType(uint256Type);
+globalContext.putType(uint512Type);
 
 globalContext.putHandler(namespaceHandler);
 
--- src/types.js
+++ src/types.js
@@ -1,8 +1,40 @@
 import { Type } from "./elements.js";
 
 export const binaryType = new Type('binary', [], { native: true });
-export const intType = new Type('int', [], { native: true });
-export const floatType = new Type('float', [], { native: true });
 export const stringType = new Type('string', [], { native: true });
 export const arrayType = new Type('array', [], { native: true });
 export const nullType = new Type('null', [], { native: true });
+export const boolType = new Type('bool', [], { native: true });
+export const anyType = new Type('any', [], { native: true });
+export const objectType = new Type('object', [], { native: true });
+
+export const numberType = new Type('number', [], { native: true });
+export const unumberType = new Type('unumber', [], { native: true });
+
+export const floatType = new Type('float', [], { native: true });
+export const float32Type = new Type('float32', [], { native: true });
+export const float64Type = new Type('float64', [], { native: true });
+export const float128Type = new Type('float128', [], { native: true });
+export const float256Type = new Type('float256', [], { native: true });
+export const float512Type = new Type('float512', [], { native: true });
+
+export const ufloatType = new Type('ufloat', [], { native: true });
+export const ufloat32Type = new Type('ufloat32', [], { native: true });
+export const ufloat64Type = new Type('ufloat64', [], { native: true });
+export const ufloat128Type = new Type('ufloat128', [], { native: true });
+export const ufloat256Type = new Type('ufloat256', [], { native: true });
+export const ufloat512Type = new Type('ufloat512', [], { native: true });
+
+export const intType = new Type('int', [], { native: true });
+export const int32Type = new Type('int32', [], { native: true });
+export const int64Type = new Type('int64', [], { native: true });
+export const int128Type = new Type('int128', [], { native: true });
+export const int256Type = new Type('int256', [], { native: true });
+export const int512Type = new Type('int512', [], { native: true });
+
+export const uintType = new Type('uint', [], { native: true });
+export const uint32Type = new Type('uint32', [], { native: true });
+export const uint64Type = new Type('uint64', [], { native: true });
+export const uint128Type = new Type('uint128', [], { native: true });
+export const uint256Type = new Type('uint256', [], { native: true });
+export const uint512Type = new Type('uint512', [], { native: true });
